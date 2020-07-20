aea95542f7b3fa3a7c3d37e3d0d2241a3645d3ac
Bryan Dollery <bryan.dollery@gmail.com>
2020-07-20 09:21:57 +0300

    Blocked all public access

--- remote-state.tf
+++ remote-state.tf
@@ -21,6 +21,15 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "sec" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = format("devops-bootcamp-locks-%s", var.name)
   billing_mode = "PAY_PER_REQUEST"
